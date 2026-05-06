4aeb2a2d03c0c5fed773c941ae8f9e7f5db97845
RidaAli110 <rida.webdev110@gmail.com>
2026-05-06 17:12:29 +0100

    Add for loop practice

---
+++ loops.js
@@ -0,0 +1,17 @@
+//For Loop
+for (let i = 1; i <=5; i++) {
+     console.log(`Number ${i}`); 
+}
+for (let i = 0; i <= 10; i += 2) {
+     console.log(i);
+     
+}
+for (let i = 10; i >= 1; i--) {
+     console.log(i);
+}
+for (let i = 1; i <= 20; i +=5) {
+     console.log(i);    
+}
+for (let i = 2; i <= 12; i+= 2) {
+     console.log(`even ${i}`);  
+}
