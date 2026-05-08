0d9e35cfa3589014cdfe4ba98bea49d3e250c671
RidaAli110 <rida.webdev110@gmail.com>
2026-05-08 18:04:02 +0100

    Add while/do while loops practice

--- loops.js
+++ loops.js
@@ -15,3 +15,26 @@ for (let i = 1; i <= 20; i +=5) {
 for (let i = 2; i <= 12; i+= 2) {
      console.log(`even ${i}`);  
 }
+
+//While/do while loop
+let count = 5;
+while (count >= 1) {
+     console.log(`countdown: ${count}`);
+     count --;  
+}
+console.log('blast off');
+
+let attempts = 1;
+do {
+     console.log(`attempt ${attempts}`);
+     attempts ++;
+} while (attempts <= 3);
+console.log('access denied');
+
+let energy = 3;
+while (energy >= 1) {
+     console.log(`energy left: ${energy}`);
+     energy --;
+}
+console.log('game over');
+
