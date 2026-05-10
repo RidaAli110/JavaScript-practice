2cf9e904403f20d63c56a17a78813af488cc3dc4
RidaAli110 <rida.webdev110@gmail.com>
2026-05-10 20:56:48 +0100

    Add function expressions practice

---
+++ functions.js
@@ -0,0 +1,14 @@
+//invoking a function
+function  myName(){
+     console.log('rida'); 
+}
+myName();
+
+//function expressions
+let colours = function(){
+     console.log('red, green, blue');
+    
+}
+colours();
+let myColours = colours;
+myColours();
