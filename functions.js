2e78734b748ca42fd8a9fc9756ee395a283c53ad
RidaAli110 <rida.webdev110@gmail.com>
2026-05-11 21:15:54 +0100

    Add functions parameters and arguments practice

--- functions.js
+++ functions.js
@@ -12,3 +12,25 @@ let colours = function(){
 colours();
 let myColours = colours;
 myColours();
+
+//Parameters and arguments
+function checkOutTotal(itemPrice, shippingCost) {
+     console.log(`Total: £${itemPrice + shippingCost}`);
+}
+checkOutTotal(10, 15);
+checkOutTotal(20, 5);
+function sendMessage(username, message) {
+     console.log(`${username} sent ${message}`);
+}
+sendMessage('Alex', 'Hello everyone!');
+function logInUser(username = 'guest', password) {
+     if (password === '12345') {
+          console.log(`Welcome back, ${username}`);
+          
+     } else {
+          console.log('Incorrect password');
+     }
+}
+logInUser('james', '12345')
+logInUser('alice', '3456')
+logInUser(undefined, '12345');
