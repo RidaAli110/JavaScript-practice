2a0e3ea24cd1d0a5a12d78576cc6f9489c70ff36
RidaAli110 <rida.webdev110@gmail.com>
2026-05-19 19:37:38 +0100

    Add looping through arrays practice

--- loops.js
+++ loops.js
@@ -75,3 +75,31 @@ let colors = ['red', 'orange', 'yellow', 'green', 'blue'];
 for (let x of colors) {
      console.log(x);   
 }
+
+//Looping in arrays
+const color = ['red', 'blu', 'green'];
+for(let i = 0; i < color.length; i++) {
+     console.log(color[i]); 
+}
+const movies = ['bee movie', 'ice age', 'madagascar', 'rio 2'];
+for(let i = 0; i < movies.length; i++){
+     console.log(i);
+}
+const numbers = [5, 10, 15];
+for(let i = 0; i < numbers.length; i++) {
+     console.log(numbers[i] + 2);
+}
+const numbers2 = [3, 6, 9, 12, 15, 18];
+for(let i = 0; i < numbers2.length; i++) {
+     if(numbers2[i] > 10) {
+          console.log(numbers2[i]);
+     }
+}
+const nums = [4, 7, 10, 13];
+for(let i = 0; i < nums.length; i++){
+     if (nums[i] % 2 === 0) {
+          console.log(`${nums[i]} is even`);  
+     } else {
+          console.log(`${nums[i]} is odd`);
+     }
+}
