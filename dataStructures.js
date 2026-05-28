7f821aa3be4b0697987dc2fc1508a40181200283
RidaAli110 <rida.webdev110@gmail.com>
2026-05-28 20:44:08 +0100

    Add destructuring barrays practice

---
+++ dataStructures.js
@@ -0,0 +1,6 @@
+// destructuring arrays practice
+const books = ['history book', 'math book', 'science book', 'english book'];
+const [firstBook, secondBook] = books;
+console.log(firstBook, secondBook);
+const[, , thirdbook] = books;
+console.log(thirdbook);
