d553c1bce7cac0a9d5a8d73c8eb05593aa7959b4
RidaAli110 <rida.webdev110@gmail.com>
2026-06-19 19:14:51 +0100

    Add ternary operator practice

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -1,3 +1,7 @@
 const age = 18;
 const status = age >= 18 ? 'adult' : 'minor';
 console.log(status);
+
+const isLoggedIn = false;
+const access = isLoggedIn ? 'Access Granted' : 'Access Denied';
+console.log(access);
