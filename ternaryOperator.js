1b6a0c7e9c3594e531828c42386351333f1f117b
RidaAli110 <rida.webdev110@gmail.com>
2026-06-29 16:51:02 +0100

    Add ternary operator excersise

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -5,3 +5,13 @@ console.log(status);
 const isLoggedIn = false;
 const access = isLoggedIn ? 'Access Granted' : 'Access Denied';
 console.log(access);
+
+const age2 = 15;
+const hasParent = true;
+const message =
+  age2 >= 18
+    ? "Enjoy the movie!"
+    : hasParent
+    ? "You can enter with a parent."
+    : "Entry denied.";
+console.log(message);
